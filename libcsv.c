2f78ed067fae583679fbbfa5c8cb0d435197cafa
Renato Miranda Gonçalves <remiranda@Renatos-MacBook-Air.local>
2024-07-09 21:26:11 -0300

    feat: add File opening to processCsvFile

--- libcsv.c
+++ libcsv.c
@@ -2,6 +2,8 @@
 #include <stddef.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 void    processCsvLine(const char csvLine[]);
 
@@ -13,7 +15,27 @@ void processCsv( const char csvData[], const char selectedColumns[], const char 
 }
 
 void processCsvFile( const char csvFilePath[], const char selectedColumns[], const char rowFilterDefinitions[] ) {
-    (void)csvFilePath;
+    FILE    *stream;
+    char    *line;
+    size_t  len;
+    ssize_t readBytes;
+
+    stream = fopen(csvFilePath, "r");
+    if (stream == NULL){
+        write(1, &"Failed to open file\n", 20);
+        return ;
+    }
+    line = NULL;
+    len = 0;
+    readBytes = getline(&line, &len, stream);
+    while (readBytes > 0) {
+        processCsvLine(line);
+        if (line)
+            free(line);
+        line = NULL;
+        readBytes = getline(&line, &len, stream);
+    }
+    // (void)csvFilePath;
     (void)selectedColumns;
     (void)rowFilterDefinitions;
     return ;
--- unitTest.c
+++ unitTest.c
@@ -1,9 +1,10 @@
 #include "unitTest.h"
 
 int main(void) {
-    const char  line[] = "test,test1,test2, test space, hi, ,";
+    const char  line[] = "col1,col2,col3,col4,col5,col6,col7\n";
 
-    processCsvFile(NULL, NULL, NULL);
+    processCsvFile("data.csverror", NULL, NULL);
+    processCsvFile("data.csv", NULL, NULL);
     processCsv(line, NULL, NULL);
     return (0);
 }
