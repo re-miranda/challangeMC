1d6bd9edb57e095792766ba0f57bf1593f1be897
Renato Miranda Gonçalves <remiranda@Renatos-MacBook-Air.local>
2024-07-11 22:52:28 -0300

    test: add multiple line test to line

--- unitTest.c
+++ unitTest.c
@@ -1,7 +1,7 @@
 #include "unitTest.h"
 
 int main(void) {
-    const char  line[] = "col1,col2,col3,col4,col5,col6,col7\n";
+    const char  line[] = "col1,col2,col3,col4,col5,col6,col7\nl1c1,l1c2,l1c3,l1c4,l1c5,l1c6,l1c7\nl1c1,l1c2,l1c3,l1c4,l1c5,l1c6,l1c7\nl2c1,l2c2,l2c3,l2c4,l2c5,l2c6,l2c7\nl3c1,l3c2,l3c3,l3c4,l3c5,l3c6,l3c7\n";
 
     processCsvFile("data.csverror", NULL, NULL);
     processCsvFile("data.csv", NULL, NULL);
