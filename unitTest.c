06f618f64c5f6751f90094a2dd87d276d85ed94b
Renato Miranda Gonçalves <remiranda@Renatos-MacBook-Air.local>
2024-07-12 23:03:04 -0300

    feat: add aditional wrong selection testing string

--- unitTest.c
+++ unitTest.c
@@ -3,6 +3,7 @@
 int main(void) {
     const char  line[] = "col1,col2,col3,col4,col5,col6,col7\nl1c1,l1c2,l1c3,l1c4,l1c5,l1c6,l1c7\nl1c1,l1c2,l1c3,l1c4,l1c5,l1c6,l1c7\nl2c1,l2c2,l2c3,l2c4,l2c5,l2c6,l2c7\nl3c1,l3c2,l3c3,l3c4,l3c5,l3c6,l3c7\n";
     const char  selHeaders[] = "col2,col3,col4,col6";
+    const char  selHeadersWrong[] = "ol2,xcol3x,xcol4x,xcol6g";
 
     processCsvFile("data.csverror", NULL, NULL);
     processCsvFile("data.csv", NULL, NULL);
@@ -11,5 +12,9 @@ int main(void) {
     processCsvFile("data.csverror", selHeaders, NULL);
     processCsvFile("data.csv", selHeaders, NULL);
     processCsv(line, selHeaders, NULL);
+
+    processCsvFile("data.csverror", selHeadersWrong, NULL);
+    processCsvFile("data.csv", selHeadersWrong, NULL);
+    processCsv(line, selHeadersWrong, NULL);
     return (0);
 }
